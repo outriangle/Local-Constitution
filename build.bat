4f7a407258e1a2e08a973e6736eb4480fe821a55
Noah Craig <noahcraig123@gmail.com>
2019-04-08 01:00:48 -0500

    CI Test

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rmdir /s/q tmp
 mkdir tmp
-pdflatex -aux-directory=tmp template.tex -jobname LocalConstitution
-pdflatex -aux-directory=tmp template.tex -jobname LocalConstitution
+pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
+pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
