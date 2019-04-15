24efefe33b9c85adb34cf10e7d46b682acfc4090
Noah Craig <noahcraig123@gmail.com>
2019-04-15 13:01:06 -0500

    Fix style issues with pdf building on CI (#5)

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rmdir /s/q tmp
 mkdir tmp
-pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
-pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
+pdflatex -aux-directory=tmp main.tex
+pdflatex -aux-directory=tmp main.tex
