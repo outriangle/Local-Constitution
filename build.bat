9a873d9486c8d84b8e5dfdb9d241fa5518b5f451
Noah Craig <noahcraig123@gmail.com>
2019-04-15 01:28:23 -0500

    Test new travis build script

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rmdir /s/q tmp
 mkdir tmp
-pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
-pdflatex -aux-directory=tmp main.tex -jobname LocalConstitutionAndBylaws
+xelatex -aux-directory=tmp main.tex
+xelatex -aux-directory=tmp main.tex
