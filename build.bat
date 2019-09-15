386f137303c93027a74bef137d63f342c809f6da
Noah Craig <noahcraig123@gmail.com>
2019-09-15 16:43:17 -0500

    Add minimum expectations document

--- build.bat
+++ build.bat
@@ -2,3 +2,5 @@ rmdir /s/q tmp
 mkdir tmp
 pdflatex -aux-directory=tmp main.tex
 pdflatex -aux-directory=tmp main.tex
+pdflatex -aux-directory=tmp MinimumExpectations.tex
+pdflatex -aux-directory=tmp MinimumExpectations.tex
